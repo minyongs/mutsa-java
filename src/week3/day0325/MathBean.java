f904a598fe9905d125f2b5d4ce64a3445846023c
Minhyung song <stylish_c@naver.com>
2024-03-25 21:32:45 +0900

    .

---
+++ src/week3/day0325/MathBean.java
@@ -0,0 +1,21 @@
+package week3.day0325;
+
+public class MathBean {
+
+    public void printClassName(){
+        System.out.println("클래스 이름은 MathBean 입니다.");
+    }
+
+    public void printNumber(int x){
+
+        System.out.println("출력값 :" + x);
+    }
+
+    public int getOne(){
+        return 1;
+    }
+
+    public int plus(int x, int y){
+        return x + y ;
+    }
+}
