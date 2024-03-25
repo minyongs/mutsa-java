007ffd72224ff5974829ae4eb9b6c0217450b643
Minhyung song <stylish_c@naver.com>
2024-03-25 22:03:40 +0900

    간단한 로또 게임 구현

--- src/week3/day0325/lotto/User.java
+++ src/week3/day0325/lotto/User.java
@@ -13,10 +13,24 @@ public class User {
         userPick();
     }
     public void userPick(){
-        for(int i = 0; i<6; i++){
+        int cnt = 0;
+        while(cnt<6){
+            System.out.println((cnt+1) + " 번째 숫자를 픽하세요(0~45)");
+            int n = sc.nextInt();
+            if(n>=1 && n <=45){
+                userPickArr[cnt] = new Ball(n);
+                cnt ++;
+            }else{
+                System.out.println("잘못된 입력입니다.다시 입력하세요.");
+            }
+
+
+
+        }
+       /* for(int i = 0; i<6; i++){
 
             System.out.println((i+1) + " 번째 숫자를 픽하세요(0~45)");
             userPickArr[i] = new Ball(sc.nextInt());
-        }
+        }*/
     }
 }
