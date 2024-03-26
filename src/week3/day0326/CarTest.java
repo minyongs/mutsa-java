284432891ca304d7749e019a23c3a5b8a49723fb
Minhyung song <stylish_c@naver.com>
2024-03-26 14:21:24 +0900

    inheritance

---
+++ src/week3/day0326/CarTest.java
@@ -0,0 +1,9 @@
+package week3.day0326;
+
+public class CarTest {
+    public static void main(String[] args) {
+        Car car2 = new Car();
+        Car car = new Car("람보르기니", 140);
+        System.out.println(car.name +" "+ car.speed);
+    }
+}
